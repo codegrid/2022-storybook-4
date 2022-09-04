14796c606e96a845a541b272a6279d092d4e0b8b
tomof <fffjt007@gmail.com>
2022-09-04 17:27:42 +0900

    引数の情報を正しく読み込むために、reactDocgen: falseの指定を削除

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
-  typescript: {
-    // 不具合の対応：https://github.com/storybookjs/storybook/issues/15336
-    reactDocgen: false,
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: '@storybook/react',
